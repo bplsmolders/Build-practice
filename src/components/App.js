c511b0b74a6614e457984a40998c42965dbc25af
bplsmolders <79159158+bplsmolders@users.noreply.github.com>
2021-03-31 12:53:12 +0200

    fixes page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 
 // App Components
 import Home from './Home';
@@ -11,7 +11,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <div className="container">
       <Header />
       
@@ -24,7 +24,7 @@ const App = () => (
         <Route component={NotFound}/>
       </Switch>
     </div>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
